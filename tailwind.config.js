17b90be961c19222ac70a257be8f4969d5ecf8d3
DemurAIdev <cyberaioff@gmail.com>
2023-05-28 19:22:10 +0700

    Reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'white': '#edf5e1',
+      'green1': '#5cdb95',
+      'green2': '#05386b',
+    }
   },
   plugins: [],
 }
